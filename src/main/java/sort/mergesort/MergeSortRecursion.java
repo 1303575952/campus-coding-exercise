ca8c536f1827913e7c950cde501a5855cfb94356
1303575952@qq.com <1303575952@qq.com>
2019-05-23 20:15:51 +0800

    归并递归

---
+++ src/main/java/sort/mergesort/MergeSortRecursion.java
@@ -0,0 +1,54 @@
+package sort.mergesort;
+
+import java.util.Arrays;
+
+public class MergeSortRecursion {
+    // 将arr[l...mid]和arr[mid+1...r]两部分进行归并
+    private static void merge(int[] arr, int l, int mid, int r) {
+
+        int[] aux = Arrays.copyOfRange(arr, l, r + 1);
+
+        // 初始化，i指向左半部分的起始索引位置l；j指向右半部分起始索引位置mid+1
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+
+            if (i > mid) {  // 如果左半部分元素已经全部处理完毕
+                arr[k] = aux[j - l];
+                j++;
+            } else if (j > r) {   // 如果右半部分元素已经全部处理完毕
+                arr[k] = aux[i - l];
+                i++;
+            } else if (aux[i - l] - aux[j - l] < 0) {  // 左半部分所指元素 < 右半部分所指元素
+                arr[k] = aux[i - l];
+                i++;
+            } else {  // 左半部分所指元素 >= 右半部分所指元素
+                arr[k] = aux[j - l];
+                j++;
+            }
+        }
+    }
+
+    // 递归使用归并排序,对arr[l...r]的范围进行排序
+    private static void sort(int[] arr, int l, int r) {
+
+        if (l >= r)
+            return;
+
+        int mid = (l + r) / 2;
+        sort(arr, l, mid);
+        sort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    public static void sort(int[] arr) {
+
+        int n = arr.length;
+        sort(arr, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 2, 3};
+        sort(arr);
+        System.out.println(Arrays.toString(arr)); // [2, 3, 5, 10]
+    }
+}
