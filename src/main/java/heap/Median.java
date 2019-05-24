8c695f730b56ba2909b60ec73f59fa0bdee902a8
1303575952@qq.com <1303575952@qq.com>
2019-05-24 16:12:28 +0800

    数据流的中位数

---
+++ src/main/java/heap/Median.java
@@ -0,0 +1,43 @@
+package heap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 数据流中的中位数
+ */
+public class Median {
+    private int count = 0;
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(15, new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    });
+
+    public void Insert(Integer num) {
+        if (count % 2 == 0) {
+            //1.新加入的元素先入到大根堆，由大根堆筛选出堆中最大的元素
+            maxHeap.offer(num);
+            int filteredMaxNum = maxHeap.poll();
+            //2.筛选后的【大根堆中的最大元素】进入小根堆
+            minHeap.offer(filteredMaxNum);
+        } else {
+            //1.新加入的元素先入到小根堆，由小根堆筛选出堆中最小的元素
+            minHeap.offer(num);
+            int filteredMinNum = minHeap.poll();
+            //2.筛选后的【小根堆中的最小元素】进入大根堆
+            maxHeap.offer(filteredMinNum);
+        }
+        count++;
+    }
+
+    public Double GetMedian() {
+        if (count % 2 == 0) {
+            return new Double((minHeap.peek() + maxHeap.peek())) / 2;
+        } else {
+            return new Double(minHeap.peek());
+        }
+    }
+}
