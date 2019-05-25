8b63bfb6634cd3fa63d974088eb29a688a7c1cda
1303575952@qq.com <1303575952@qq.com>
2019-05-25 15:56:34 +0800

    单例模式

---
+++ src/main/java/designpattern/singleton/doublechecklock/Singleton.java
@@ -0,0 +1,20 @@
+package designpattern.singleton.doublechecklock;
+
+public class Singleton {
+
+    private volatile static Singleton uniqueInstance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getUniqueInstance() {
+        if (uniqueInstance == null) {
+            synchronized (Singleton.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+}
---
+++ src/main/java/designpattern/singleton/innerclass/Singleton.java
@@ -0,0 +1,15 @@
+package designpattern.singleton.innerclass;
+
+public class Singleton {
+
+    private Singleton() {
+    }
+
+    private static class SingletonHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static Singleton getUniqueInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+}
