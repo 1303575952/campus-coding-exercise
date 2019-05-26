e54f8b822d502d996b403b8f2144d78db89b6030
1303575952@qq.com <1303575952@qq.com>
2019-05-26 12:31:18 +0800

    装饰者模式

---
+++ src/main/java/designpattern/decorator/ABattercake.java
@@ -0,0 +1,11 @@
+package designpattern.decorator;
+
+/**
+ * Created by lifei
+ */
+public abstract class ABattercake {
+    protected abstract String getDesc();
+
+    protected abstract int cost();
+
+}
---
+++ src/main/java/designpattern/decorator/EggDecorator.java
@@ -0,0 +1,25 @@
+package designpattern.decorator;
+
+/**
+ * Created by lifei
+ */
+public class EggDecorator extends AbstractDecorator {
+    public EggDecorator(ABattercake aBattercake) {
+        super(aBattercake);
+    }
+
+    @Override
+    protected void doSomething() {
+
+    }
+
+    @Override
+    protected String getDesc() {
+        return super.getDesc() + " 加一个鸡蛋";
+    }
+
+    @Override
+    protected int cost() {
+        return super.cost() + 1;
+    }
+}
