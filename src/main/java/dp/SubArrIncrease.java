826f8a8c765b97b085bba335212791529355c350
1303575952 <1303575952@qq.com>
2019-08-16 09:32:38 +0800

    最长递增子序列

--- src/main/java/dp/SubArrIncrease.java
+++ src/main/java/dp/SubArrIncrease.java
@@ -1,5 +1,7 @@
 package dp;
 
+import java.util.Arrays;
+
 /**
  * 最长递增子序列
  * <p>
@@ -14,20 +16,15 @@ public class SubArrIncrease {
         for (int j = 0; j < arr.length; j++) {
             for (int i = 0; i < j; i++) {
                 if (arr[j] > arr[i]) {
-                    longest[j] = longest[i] + 1;
+                    longest[j] = longest[j] > (longest[i] + 1) ? longest[j] : (longest[i] + 1);
                 }
             }
         }
-        int max = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (longest[i] > max) {
-                max = longest[i];
-            }
-        }
-        return max;
+        System.out.println(Arrays.toString(longest));
+        return longest[longest.length - 1];
     }
 
     public static void main(String[] args) {
-        System.out.println(new SubArrIncrease().getLength(new int[]{1, 3, 5, 2, 4, 6, 7, 8}));
+        System.out.println(new SubArrIncrease().getLength(new int[]{1, 3, 5, 2, 1, 4, 6, 7, 8}));
     }
 }
