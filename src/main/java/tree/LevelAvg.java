7dfc54939f47c3ee3b1e30cd52bc828430dde3dc
1303575952 <1303575952@qq.com>
2019-09-19 09:04:26 +0800

    每层平均值

---
+++ src/main/java/tree/LevelAvg.java
@@ -0,0 +1,30 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给定一个非空二叉树, 返回一个由每层节点平均值组成的数组
+ */
+public class LevelAvg {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> list = new LinkedList<>();
+        list.add(root);
+        while (list.size() != 0) {
+            int len = list.size();
+            double sum = 0;
+            for (int i = 0; i < len; i++) {
+                TreeNode node = list.poll();
+                sum += node.val;
+                if (node.left != null) list.add(node.left);
+                if (node.right != null) list.add(node.right);
+            }
+            res.add(sum / len);
+        }
+        return res;
+    }
+}
